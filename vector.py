00b1def69fe985052c9a3d77c5fae809f50eb805
Anisio Marques Junior <anisiomarxjr@gmail.com>
2015-06-06 16:06:51 -0400

    new method to multiply

--- vector.py
+++ vector.py
@@ -4,7 +4,10 @@ class Vector:
         self.vector = v
 
     def __add__(self,v):
-        [self.vector[i] + v[i] for i in range(leng(self.v))] if len(self.vector) == len(v) else []
+        return [self.vector[i] + v.vector[i] for i in range(len(self.vector))] if len(self.vector) == len(v.vector) else []
 
     def __sub__(self,v):
-        [self.vector[i] - v[i] for i in range(leng(self.v))] if len(self.vector) == len(v) else []
+        return [self.vector[i] - v.vector[i] for i in range(len(self.vector))] if len(self.vector) == len(v.vector) else []
+
+    def __mul__(self,v):
+        return [self.vector[i] * v.vector[i] for i in range(len(self.vector))] if len(self.vector) == len(v.vector) else []
